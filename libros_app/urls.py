156188607b7c1640bde4db2554bb105ae6ae6fd2
TaladropoliS <fernando.taladriz@gmail.com>
2021-02-20 12:00:35 -0300

    actualizando html

--- libros_app/urls.py
+++ libros_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.inicio),
+    path('autores', views.autores),
+    path('ver_autor', views.ver_autor),
+    path('ver_libro', views.ver_libro),
 ]
--- libros_app/views.py
+++ libros_app/views.py
@@ -4,10 +4,29 @@ from django.contrib import messages
 # Create your views here.
 
 def inicio(request):
-
     libros = Libro.objects.all()
     context = {
         "libros": Libro.objects.all()
     }
-
     return render(request, 'index.html', context)
+
+def autores(request):
+    autores = Libro.objects.all()
+    context = {
+        "libros": Libro.objects.all()
+    }
+    return render(request, 'autores.html', context)
+
+def ver_autor(request):
+    autores = Libro.objects.all()
+    context = {
+        "libros": Libro.objects.all()
+    }
+    return render(request, 'ver_autor.html', context)
+
+def ver_libro(request):
+    autores = Libro.objects.all()
+    context = {
+        "libros": Libro.objects.all()
+    }
+    return render(request, 'ver_libro.html', context)
