757fb5aa6dfa054ef6ca649c198290a517a0d6d5
TaladropoliS <fernando.taladriz@gmail.com>
2021-02-23 08:59:44 -0300

    ver libro ok

--- libros_app/urls.py
+++ libros_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.inicio),
+    path('agregar_libro', views.agregar_libro),
     path('autores', views.autores),
     path('ver_autor', views.ver_autor),
     path('ver_libro', views.ver_libro),
+    path('ver_libro/<int:id>', views.ver_libro),
 ]
--- libros_app/views.py
+++ libros_app/views.py
@@ -4,29 +4,62 @@ from django.contrib import messages
 # Create your views here.
 
 def inicio(request):
+    if 'inicio' in request.session: # ve si ya se ha entrado a la sesión.
+
+        error = {}  # se crea vacío para poder enviar mensaje de error más abajo.
+        if 'titulo' in request.POST:
+            if len(request.POST['titulo']) > 0:  # verifica si 'titulo' esta vacío.
+                # if 'titulo' in request.POST:
+                # print(request.POST['titulo'])
+                add_libro = Libro.objects.create(  # se crea el libro y la descripción
+                    titulo=request.POST['titulo'],
+                    desc=request.POST['desc'],
+                )
+            else:  # se comprueba el error y se entrega un mensaje.
+                error['titulo'] = "* * * | | | El título no ha sido ingresado | | | * * *"
+            if len(error) > 0:
+                for key, msg in error.items():
+                    messages.error(request, msg)
+                # return redirect('/')
+    request.session['inicio'] = True
+
     libros = Libro.objects.all()
     context = {
         "libros": Libro.objects.all()
     }
     return render(request, 'index.html', context)
 
+# def agregar_libro(request):
+#     Libro.objects.create(titulo=request.POST['titulo'], desc=request.POST['desc'])
+#     autores = Libro.objects.all()
+#     context = {
+#         "autores": Libro.objects.all()
+#     }
+#     return render(request, 'index.html', context)
+
+def agregar_libro(request):
+    Libro.objects.create(titulo=request.POST['titulo'], desc=request.POST['desc'])
+    return render(redirect(''))
+
 def autores(request):
     autores = Libro.objects.all()
     context = {
-        "libros": Libro.objects.all()
+        "autores": Libro.objects.all()
     }
     return render(request, 'autores.html', context)
 
 def ver_autor(request):
-    autores = Libro.objects.all()
+    a = Libro.objects.id(id=id)
     context = {
-        "libros": Libro.objects.all()
+        "autores": a
     }
     return render(request, 'ver_autor.html', context)
 
-def ver_libro(request):
-    autores = Libro.objects.all()
+def ver_libro(request,id):
+    temp = Libro.objects.get(id=id)
     context = {
-        "libros": Libro.objects.all()
+        "titulo": temp.titulo,
+        "id": temp.id,
+        "desc": temp.desc,
     }
     return render(request, 'ver_libro.html', context)
